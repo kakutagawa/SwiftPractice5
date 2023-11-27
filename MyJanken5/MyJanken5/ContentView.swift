1d12ece65548ffd2d268c8a8ccf13861a8963b7a
kakutagawa <kaktgw0824@gmail.com>
2023-11-27 23:01:53 +0900

    initial commit

---
+++ MyJanken5/MyJanken5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJanken5
+//
+//  Created by 芥川浩平 on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJanken5/MyJanken5/MyJanken5App.swift
@@ -0,0 +1,17 @@
+//
+//  MyJanken5App.swift
+//  MyJanken5
+//
+//  Created by 芥川浩平 on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MyJanken5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

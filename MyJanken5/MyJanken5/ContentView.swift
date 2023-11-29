2378398f2e34c4eb1de66aabb26bce26c6c20f03
kakutagawa <kaktgw0824@gmail.com>
2023-11-29 23:16:32 +0900

    MyJanken5 OK

--- MyJanken5/MyJanken5/ContentView.swift
+++ MyJanken5/MyJanken5/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+enum Janken:String, CaseIterable {
+    case gu
+    case choki
+    case pa
+
+    var text: String {
+        switch self {
+        case .gu:
+            "グー"
+        case .choki:
+            "チョキ"
+        case .pa:
+            "パー"
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var answer: Janken?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+
+            if let answer {
+                Image(answer.rawValue)
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+                Text(answer.text)
+            }
+
+            if answer == nil {
+                Text("これからじゃんけんをします")
+            }
+
+            Button {
+                var newAnswer: Janken?
+                repeat {
+                    newAnswer = Janken.allCases.randomElement()
+                } while answer == newAnswer
+                answer = newAnswer
+            } label: {
+                Text("じゃんけんをする！")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(.pink)
+                    .foregroundColor(.white)
+            }
         }
-        .padding()
     }
 }
 

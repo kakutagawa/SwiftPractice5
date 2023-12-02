fa1143aa137caecc3d7bbffb6f66f702eeb648a7
kakutagawa <kaktgw0824@gmail.com>
2023-12-02 23:16:42 +0900

    MyTimer5 OK

---
+++ MyTimer5/MyTimer5/MyTimer5App.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimer5App.swift
+//  MyTimer5
+//
+//  Created by 芥川浩平 on 2023/12/02.
+//
+
+import SwiftUI
+
+@main
+struct MyTimer5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
